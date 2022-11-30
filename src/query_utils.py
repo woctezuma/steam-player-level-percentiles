82dbd037a1534915ad768d2453019a595ba22ce1
Wok <woctezuma@users.noreply.github.com>
2022-11-30 20:51:42 +0100

    Add query utils

---
+++ src/query_utils.py
@@ -0,0 +1,20 @@
+import requests
+
+API_BASE_URL = "https://api.steampowered.com"
+API_ENDPOINT = "/IPlayerService/GetSteamLevelDistribution/v1/"
+API_URL = f"{API_BASE_URL}{API_ENDPOINT}"
+
+
+def get_params(access_token, player_level):
+    return {"access_token": access_token, "player_level": player_level}
+
+
+def query_player_level_percentile(params):
+    response = requests.get(url=API_URL, params=params)
+
+    if response.ok:
+        player_level_percentile = response.json()["response"]["player_level_percentile"]
+    else:
+        player_level_percentile = None
+
+    return player_level_percentile
