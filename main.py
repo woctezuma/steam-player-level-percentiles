1ceafcb043373a94275d2fff03540747055e31d8
Wok <woctezuma@users.noreply.github.com>
2022-11-30 21:05:34 +0100

    Toggle OFF the update of player level percentiles

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from src.data_utils import load_json, TRIMMED_PERCENTILE_FNAME
 from src.download_utils import download_player_level_percentiles
 from src.math_utils import compute_denominators_from_dict
 
 
 def main():
-    d = download_player_level_percentiles(start_level=1, end_level=5000)
+    force_update = False
+
+    if force_update:
+        d = download_player_level_percentiles(start_level=1, end_level=5000)
+    else:
+        d = load_json(TRIMMED_PERCENTILE_FNAME)
     n = compute_denominators_from_dict(d, max_input_value=100)
 
     print(f"Least Common Multiple: {n}")
