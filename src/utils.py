2d6195e45ca605c67e50bdeb61967999515bf9e1
Wok <woctezuma@users.noreply.github.com>
2022-12-01 01:45:37 +0100

    Remove inconsistent values from dict

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-def remove_duplicate_values_from_dict(d):
+def remove_duplicate_values_from_dict(d, threshold=100):
     trimmed_d = {}
     temp_value = None
 
@@ -7,6 +7,10 @@ def remove_duplicate_values_from_dict(d):
 
     for k in sorted_keys:
         v = d[k]
+
+        if threshold is not None and v > threshold:
+            # Skip inconsistent values, e.g. a percentile higher than 100
+            continue
 
         if temp_value is None or v != temp_value or k == last_key:
             trimmed_d[k] = v
