36c00e35e4528d1ac4157ce08747e02e8f9daba1
Wok <woctezuma@users.noreply.github.com>
2022-12-01 10:21:52 +0100

    Query achievement unlock percentages

--- src/query_utils.py
+++ src/query_utils.py
@@ -2,6 +2,7 @@ import requests
 
 API_BASE_URL = "https://api.steampowered.com"
 LEVEL_ENDPOINT = "/IPlayerService/GetSteamLevelDistribution/v1/"
+ACHIEVEMENT_ENDPOINT = "/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/"
 
 
 def get_params(access_token, player_level):
@@ -18,3 +19,16 @@ def query_player_level_percentile(params):
         player_level_percentile = None
 
     return player_level_percentile
+
+
+def query_achievement_percentages(app_id):
+    api_url = f"{API_BASE_URL}{ACHIEVEMENT_ENDPOINT}"
+    params = {"gameid": app_id}
+    response = requests.get(url=api_url, params=params)
+
+    if response.ok:
+        achievements = response.json()["achievementpercentages"]["achievements"]
+    else:
+        achievements = None
+
+    return achievements
