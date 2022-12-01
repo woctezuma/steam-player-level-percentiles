7165bb718e68a5acd690aa18c8ba17c35a883b96
Wok <woctezuma@users.noreply.github.com>
2022-12-01 10:16:59 +0100

    Minor refactoring: move constant to variable

--- src/query_utils.py
+++ src/query_utils.py
@@ -2,7 +2,6 @@ import requests
 
 API_BASE_URL = "https://api.steampowered.com"
 LEVEL_ENDPOINT = "/IPlayerService/GetSteamLevelDistribution/v1/"
-API_URL = f"{API_BASE_URL}{LEVEL_ENDPOINT}"
 
 
 def get_params(access_token, player_level):
@@ -10,7 +9,8 @@ def get_params(access_token, player_level):
 
 
 def query_player_level_percentile(params):
-    response = requests.get(url=API_URL, params=params)
+    api_url = f"{API_BASE_URL}{LEVEL_ENDPOINT}"
+    response = requests.get(url=api_url, params=params)
 
     if response.ok:
         player_level_percentile = response.json()["response"]["player_level_percentile"]
